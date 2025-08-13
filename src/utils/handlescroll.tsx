15fd6756f0016991393569b72f1dc2f723052ea6
Cyaside <tristan.rasheedsatria@gmail.com>
2025-08-13 22:49:28 +0700

    Everything Start From Somewhere

---
+++ src/utils/handlescroll.tsx
@@ -0,0 +1,13 @@
+export const handleScroll = (elementId: string) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+};
+
+export const useScrollToSection = () => {
+  return handleScroll;
+};
